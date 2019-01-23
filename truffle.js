ab8637108846bdb6e9e5b467a121633fe28d794f
Matt Lally <mattlally@Matts-MacBook-Pro.local>
2019-01-23 17:46:43 -0500

    Hacking.

--- truffle.js
+++ truffle.js
@@ -1,14 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
     }
-  },
-  compilers: {
-     solc: {
-       version: "^0.4.22"  // ex:  "0.4.20". (Default: Truffle's installed solc)
-     }
   }
+  // ,
+  // compilers: {
+  //    solc: {
+  //      version: "^0.4.22"  // ex:  "0.4.20". (Default: Truffle's installed solc)
+  //    }
+  // }
 };
